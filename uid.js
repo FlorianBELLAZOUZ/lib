e8e6b0b7e48826447d20831b422792f2aaec8064
florian Bellazouz <florian.bellazouz@gmail.com>
2018-05-21 10:40:48 +0200

    add uid

---
+++ uid.js
@@ -0,0 +1,20 @@
+const {alphabet:Alphabet,int,fixed} = require('./alphabet')
+const {times} = require('./funcs')
+
+const fromCharCode = a=>String.fromCharCode(a)
+const AZ=times(26,i=>65+i).map(fromCharCode)
+const az=times(26,i=>97+i).map(fromCharCode)
+const ten=times(10,i=>i).map(String)
+
+const alphabet = AZ.concat(az,ten)
+const rand = ()=>{
+  const val = Alphabet(Math.floor(Math.random()*15018570),alphabet)
+  return fixed(4,val,alphabet)}
+const ms = ()=>{
+  const val = Alphabet(Date.now(),alphabet)
+  return fixed(8,val,alphabet)}
+
+const uid = ()=>rand()+ms()
+const ms = id=>int(id.slice(4),alphabet)
+
+module.exports = uid
