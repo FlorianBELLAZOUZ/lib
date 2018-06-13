862dc57323395404a52f96d05016230848d9c0c7
florianBELLAZOUZ <florian.bellazouz@gmail.com>
2018-06-13 23:11:02 +0200

    update string - change cipher

--- string.js
+++ string.js
@@ -3,10 +3,10 @@ const crypto = require('crypto')
 const utf = 'utf8'
 const ltn = 'latin1'
 const encrypt = (key,a)=>{
-  const cc = crypto.createCipher('chacha20',key)
+  const cc = crypto.createCipher('aes192',key)
   return cc.update(a,utf,ltn)+cc.final(ltn) }
 const decrypt = (key,a)=>{
-  const dc = crypto.createDecipher('chacha20',key)
+  const dc = crypto.createDecipher('aes192',key)
   return dc.update(a,ltn,utf)+dc.final(utf) }
 
 module.exports = {encrypt,decrypt}
