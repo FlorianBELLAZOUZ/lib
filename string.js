80e379e99570ec7d19af4ef2169154d0ff37062f
florianBELLAZOUZ <florian.bellazouz@gmail.com>
2018-06-13 23:18:10 +0200

    update string - base64

--- string.js
+++ string.js
@@ -1,12 +1,12 @@
 const crypto = require('crypto')
 
 const utf = 'utf8'
-const ltn = 'latin1'
+const base = 'base64'
 const encrypt = (key,a)=>{
   const cc = crypto.createCipher('aes192',key)
-  return cc.update(a,utf,ltn)+cc.final(ltn) }
+  return cc.update(a,utf,base)+cc.final(base) }
 const decrypt = (key,a)=>{
   const dc = crypto.createDecipher('aes192',key)
-  return dc.update(a,ltn,utf)+dc.final(utf) }
+  return dc.update(a,base,utf)+dc.final(utf) }
 
 module.exports = {encrypt,decrypt}
