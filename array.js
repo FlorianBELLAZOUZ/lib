4fefe63625a28c9327e73c363b37658287b58f1f
florianBELLAZOUZ <florian.bellazouz@gmail.com>
2018-07-16 12:02:00 +0200

    add array - flat

--- array.js
+++ array.js
@@ -1,3 +1,5 @@
+const flat = a=>a.reduce((a,el)=>a.concat(Array.isArray(el)?flat(el):el),[])
+const times = (a,func)=>(new Array(a)).fill('').map((a,i)=>func(i))
 const uniq = a=>a.filter((el,i)=>a.indexOf(el)===i)
 
-module.exports = {uniq}
+module.exports = {uniq,flat,times}
--- func.js
+++
@@ -1,3 +0,0 @@
-const times = (a,func)=>(new Array(a)).fill('').map((a,i)=>func(i))
-
-module.exports = {times}
