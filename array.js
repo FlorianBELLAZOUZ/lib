88481a5b21518eacf9e8d36089fcf0f294f5229e
FlorianBELLAZOUZ <florian.bellazouz@gmail.com>
2019-02-11 17:12:24 +0100

    update array/times

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 const {is:o,equal:oe} = require('./object')
 
 const flat = a=>a.reduce((a,el)=>a.concat(Array.isArray(el)?flat(el):el),[])
-const times = (a,func)=>{let i=0;let o=[];while(i++<a){o.push(func(i))};return o}
+const times = (a,func)=>{let i=0;let o=[];while(i<a){o.push(func(i++))};return o}
 const uniq = a=>a.filter((el,i)=>a.indexOf(el)===i)
 const equal = (a,b)=>a.every((el,i)=>o(el)?oe(el,b[i]):el==b[i])
 
