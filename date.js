1af7c24fbab0c7c82c58b11010d416f9cf37dc4e
FlorianBELLAZOUZ <florian.bellazouz@gmail.com>
2019-03-05 15:28:04 +0100

    update date - use date instead today

--- date.js
+++ date.js
@@ -3,8 +3,8 @@ const today = ()=>{
   return d.getFullYear()*10000+(d.getMonth()+1)*100+d.getDate() }
 
 const weeks = date=>{
-  const t = (date||today())+''
-  const d = new Date(`${t.slice(0,4)}-${t.slice(4,6)}-${t.slice(6,8)}`)
+  const d = (date||new Date())
+  d.setHours(0, 0, 0, 0)
   d.setDate(d.getDate()+3-(d.getDay()+6)%7) // Thursday in current week decides the year
   const w1 = new Date(d.getFullYear(),0,4) // January 4 is always in week1
   // Adjust to Thursday in week1 and count number of weeks from d to w1
