9b3f45c62418d05ed6e045539278de91ba4826b8
FlorianBELLAZOUZ <florian.bellazouz@gmail.com>
2019-03-14 12:02:07 +0100

    update *

--- http.js
+++ http.js
@@ -1,27 +1,26 @@
 const {request:https} = require('https')
 const {request:http} = require('http')
-const {assign} = Object
 const Url = require('url')
+const {assign} = Object
 
-const opts = (method,url,data)=>{
-  const headers = data&&{
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(JSON.stringify(data)) }
-  return assign(Url.parse(url),{method}) }
-
-const fetch = method=>(url,data)=>
+const fetch = method=>(url,data,headers,opts)=>
   new Promise((suc,rej)=>{
     let body = ''
     const response = res=>{
       res.on('data',c=>body+=c)
-      res.on('end',()=>suc(JSON.parse(body))) }
-    const options = opts(method,url,data)
+      res.on('end',()=>suc(body)) }
+    const options = assign(Url.parse(url),{method,headers},opts)
     const request = options.protocol==='http:'?http:https
     const req = request(options,response)
     req.on('error',rej)
-    data&&req.write(JSON.stringify(data))
+    data&&req.write(data)
     req.end() })
-const get = fetch('GET')
-const post = fetch('POST')
+const get = fetch('get')
+const post = fetch('post')
 
-module.exports = {fetch,get,post}
+const json = method=>(url,data,headers,opts)=>{
+  const d = JSON.stringify(data)
+  const h = {'Content-Length':d.length}
+  return fetch(method)(url,d,assign(h,headers),opts).then(b=>b&&JSON.parse(b))}
+
+module.exports = {fetch,get,post,json:{get:json('get'),post:json('post')}}
--- test/array.js
+++ test/array.js
@@ -10,3 +10,6 @@ console.log('.equal(a,b)')
 it('should be equal',()=>equal(array.equal(a,b),true))
 it('should not be equal',()=>equal(array.equal(a,c),false))
 it('should not be equal',()=>equal(array.equal(a,d),false))
+
+console.log('.remove(a,f)')
+it('should remove',()=>equal(array.remove([1,2,3,4,5,6],e=>e%2==0),[1,3,5]))
