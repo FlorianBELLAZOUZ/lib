f10488989f19c8bc5e76803068d03b8c0f740efb
florianbellazouz <florian.bellazouz@gmail.com>
2019-04-02 08:56:56 +0200

    add array - remove

--- array.js
+++ array.js
@@ -4,9 +4,12 @@ const flat = a=>a.reduce((a,el)=>a.concat(Array.isArray(el)?flat(el):el),[])
 const times = (a,func)=>{let i=0;let o=[];while(i<a){o.push(func(i++))};return o}
 const uniq = a=>a.filter((el,i)=>a.indexOf(el)===i)
 const equal = (a,b)=>a.every((el,i)=>o(el)?oe(el,b[i]):el==b[i])
-const remove = (a,func)=>{
+const removes = (a,func)=>{
   for(var i=a.length-1;i>=0;i--)
     if(func(a[i])) a.splice(a.indexOf(a[i]),1)
   return a }
+const remove = (array,el)=>{
+  const i = array.indexOf(el)
+  if(i!=-1) array.splice(i,1) }
 
-module.exports = {uniq,flat,times,equal,remove}
+module.exports = {uniq,flat,times,equal,removes,remove}
--- test/array.js
+++ test/array.js
@@ -11,5 +11,5 @@ it('should be equal',()=>equal(array.equal(a,b),true))
 it('should not be equal',()=>equal(array.equal(a,c),false))
 it('should not be equal',()=>equal(array.equal(a,d),false))
 
-console.log('.remove(a,f)')
-it('should remove',()=>equal(array.remove([1,2,3,4,5,6],e=>e%2==0),[1,3,5]))
+console.log('.removes(a,f)')
+it('should removes',()=>equal(array.removes([1,2,3,4,5,6],e=>e%2==0),[1,3,5]))
