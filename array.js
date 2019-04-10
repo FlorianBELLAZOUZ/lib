6947ae063f4317c340dfcc23621f45ed41ebb1d3
florianBellazouz <florian.bellazouz@gmail.com>
2019-04-10 09:38:51 +0200

    add array - groupBy

--- array.js
+++ array.js
@@ -14,5 +14,23 @@ const remove = (a,el)=>{
 const each = (a,func)=>{
   for(var i=a.length-1;i>=0;i--){
     func(a[i])} }
+const groupBy = (a,func)=>{
+  let temp = a.slice()
+  let groups = []
+  let i = temp.length-1
+  while(i>=0){
+    let y = groups.length-1
+    while(y>=0){
+      const el = groups[y]&&groups[y][0]
+      if(func(temp[i])==func(el)){
+        groups[y].push(temp[i])
+        temp.splice(i,1)
+        break }
+      y-- }
+    if(temp[i]&&y==-1) {
+      groups.push([temp[i]])
+      temp.splice(i,1) }
+      i-- }
+  return groups }
 
-module.exports = {uniq,flat,times,equal,removes,remove,each}
+module.exports = {uniq,flat,times,equal,removes,remove,each,groupBy}
--- test/array.js
+++ test/array.js
@@ -1,6 +1,9 @@
 const {it,equal} = require('../test')
 const array = require('../array')
 
+const test = (func,ios)=>{
+  ios.forEach(([i,o])=>it(`${i} should equal ${o}`,()=>equal(func(...i),o))) }
+
 const a = [{a:'1'},{b:2},{c:true}]
 const b = [{a:'1'},{b:2},{c:true}]
 const c = [{a:'2'},{b:2},{c:true}]
@@ -13,3 +16,11 @@ it('should not be equal',()=>equal(array.equal(a,d),false))
 
 console.log('.removes(a,f)')
 it('should removes',()=>equal(array.removes([1,2,3,4,5,6],e=>e%2==0),[1,3,5]))
+
+console.log('.groupBy(a,f)')
+let ios = [
+  [[[1,2,2,4,5],a=>a],[[5],[4],[2,2],[1]]],
+  [[[1,2,2,4,5],a=>a*2],[[5],[4],[2,2],[1]]],
+  [[[{a:1},{a:2},{a:2},{a:4},{a:5}],a=>a.a],[[{a:5}],[{a:4}],[{a:2},{a:2}],[{a:1}]]], ]
+
+test(array.groupBy,ios)
