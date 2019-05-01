2734b37bf09163f94c3f6c63612b90ca5f4fd880
florianBellazouz <florian.bellazouz@gmail.com>
2019-05-01 17:16:33 +0200

    add date.hours

--- date.js
+++ date.js
@@ -1,6 +1,14 @@
+const u = undefined
+
+const hours = timeZone=>{
+  const d = new Date()
+  const opts = {timeZone,hour12:false}
+  const [_,hours] = d.toLocaleString(u,opts).split(' ').split(':')
+  return hours }
+
 const today = timeZone=>{
-  const dt = new Date()
-  const [m,d,y]=dt.toLocaleDateString(undefined,{timeZone}).split('/')
+  const d = new Date()
+  const [m,d,y]=d.toLocaleDateString(u,{timeZone}).split('/')
   return y*10000+m*100+d*1 }
 
 const weeks = date=>{
@@ -11,4 +19,4 @@ const weeks = date=>{
   // Adjust to Thursday in week1 and count number of weeks from d to w1
   return 1+Math.round(((d-w1)/86400000-3+(w1.getDay()+6)%7)/7) }
 
-module.exports={today,weeks}
+module.exports={today,weeks,hours}
