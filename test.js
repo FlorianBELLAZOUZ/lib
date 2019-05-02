fb48d0ea1284630dd91f375989b75d4e706a3dfa
florianBellazouz <florian.bellazouz@gmail.com>
2019-05-02 08:12:54 +0200

    add test - ios

--- test.js
+++ test.js
@@ -8,5 +8,7 @@ const stack = e=>e.stack.split('\n')[1].trim()
 const fail = e=>write(' \x1b[31mfail : ' + e.message+' '+stack(e)+end)
 const done = ()=>write(' \x1b[32mdone'+end)
 const it = (name,func)=>(write('\x1b[30m\x1b[1m'+name),test(func,done,fail))
+const ios = (func,ios)=>{
+  ios.forEach(([i,o])=>it(`${i} should equal ${o}`,()=>equal(func(...i),o))) }
 
-module.exports = {it,equal}
+module.exports = {it,equal,ios}
