28fccaf22657993ea0b1baa2789d700f0bda3bb1
sebovzeoueb <sebyihi@yahoo.fr>
2019-06-19 12:22:11 +0200

    fix stripe convert

--- stripeConvert.js
+++ stripeConvert.js
@@ -1,5 +1,5 @@
 const zeroDecimals = ['bif','clp','djf','gnf','jpy','kmf','krw','mga','pyg','rwf','vnd','vuv','xaf','xof','xpf']
-const toCurrency = (amount,curr) => zeroDecimals.find(curr)?amount:amount/100
-const toStripe = (value,curr) => zeroDecimals.find(curr)?value:value*100
+const toCurrency = (amount,curr) => zeroDecimals.includes(curr)?amount:amount/100
+const toStripe = (value,curr) => zeroDecimals.includes(curr)?value:value*100
 
 module.exports={toCurrency,toStripe}
