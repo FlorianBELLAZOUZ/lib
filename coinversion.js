c18b5b734a3e0401351bce00079638da62af2b2e
sebovzeoueb <sebyihi@yahoo.fr>
2019-07-02 17:47:55 +0200

    round cointributor amount to avoid crediting fractional amounts

--- coinversion.js
+++ coinversion.js
@@ -3,7 +3,7 @@ const convert = (value,tiers,price) => {
   let tier
   for(tier=0;tier<prices.length;tier++){
     let price=prices[tier]
-    if(price>=value)return tiers[tier]*2*(value/prices[tier+1])}
-  return tiers[tiers.length-1]*(value/prices[tiers.length-1])}
+    if(price>=value)return Math.round(tiers[tier]*2*(value/prices[tier+1]))}
+  return Math.round(tiers[tiers.length-1]*(value/prices[tiers.length-1]))}
 
 module.exports = convert
