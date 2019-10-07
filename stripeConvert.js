d0ebd90c690d7ff1266602f35ce5bd7eecf77e7c
florianBellazouz <florian.bellazouz@gmail.com>
2019-10-07 13:25:27 +0200

    remove coinversion&stripConvert - unuse

--- stripeConvert.js
+++
@@ -1,5 +0,0 @@
-const zeroDecimals = ['bif','clp','djf','gnf','jpy','kmf','krw','mga','pyg','rwf','vnd','vuv','xaf','xof','xpf']
-const toCurrency = (amount,curr) => zeroDecimals.includes(curr)?amount:amount/100
-const toStripe = (value,curr) => zeroDecimals.includes(curr)?value:value*100
-
-module.exports={toCurrency,toStripe}
